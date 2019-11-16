42175d75291f65b99c2c38d696d1d22b4558a1b5
neternefer <alkarinqe@yahoo.com>
2019-11-16 20:37:45 +0100

    Update arithmeticSum.js

--- javascript/arithmeticSum.js
+++ javascript/arithmeticSum.js
@@ -1,8 +1,7 @@
 /*Return the sum of the first (n) elements of a sequence
 in which each element is the sum of the given integer (a),
 and a number of occurences of the given integer (r), based
-on the element's position within the sequence.
-Read more about it
+on the element's position within the sequence. 
 */
 function ArithmeticSequenceSum(a, r, n) {
   return n / 2 * (2 * a + (n - 1) * r);
