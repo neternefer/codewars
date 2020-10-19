404546a862ddbbb8b3319b6cba3088126bae7cfc
Anna Nodzyńska <nodzynska@gmail.com>
2020-10-19 18:29:29 +0200

    Delete goals.py

--- goals.py
+++
@@ -1,7 +0,0 @@
-def goals(laLiga, copaDelRey, championsLeague):
-    """Complete the function to return the total number of
-    goals in all three leagues.
-    Fun solution: def goals(*a):
-                        return sum(a)
-    """
-    return laLiga + copaDelRey + championsLeague
