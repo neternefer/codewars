8050db7dba8c6067064506d296718ce8e8c7232e
neternefer <alkarinqe@yahoo.com>
2020-10-19 18:23:39 +0200

    Add goals kata

---
+++ goals.py
@@ -0,0 +1,7 @@
+def goals(laLiga, copaDelRey, championsLeague):
+    """Complete the function to return the total number of
+    goals in all three leagues.
+    Fun solution: def goals(*a):
+                        return sum(a)
+    """
+    return laLiga + copaDelRey + championsLeague
