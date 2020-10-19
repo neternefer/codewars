222664f2c016a18817c1756307413506ef929815
neternefer <alkarinqe@yahoo.com>
2020-10-19 18:21:00 +0200

    Add greet kata

---
+++ greet.py
@@ -0,0 +1,6 @@
+def greet(name):
+    """Make a function that will return a greeting
+       statement that uses an input; your program should return,
+       "Hello, <name> how are you doing today?".
+    """
+    return "Hello, " + name + " how are you doing today?"
