8e90b85b50443891f1cf1f69db6de35386c9d651
Anna Nodzyńska <nodzynska@gmail.com>
2020-10-19 18:29:44 +0200

    Delete greet.py

--- greet.py
+++
@@ -1,6 +0,0 @@
-def greet(name):
-    """Make a function that will return a greeting
-       statement that uses an input; your program should return,
-       "Hello, <name> how are you doing today?".
-    """
-    return "Hello, " + name + " how are you doing today?"
