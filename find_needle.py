df19d3b41a2e5ff25e3db5cf34331938d4fa6a3a
neternefer <nodzynska@gmail.com>
2020-10-21 18:37:17 +0200

    Add find_needle kata

---
+++ find_needle.py
@@ -0,0 +1,4 @@
+def find_needle(haystack):
+    """Find one needle in the array and
+    return the message 'found the needle at position x'"""
+    return 'found the needle at position ' + str(haystack.index('needle'))
