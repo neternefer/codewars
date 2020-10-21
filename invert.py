8f4a1ebf1f5979916c4b37488453733de3e8db73
neternefer <nodzynska@gmail.com>
2020-10-21 20:07:03 +0200

    Add invert.py kata

---
+++ invert.py
@@ -0,0 +1,5 @@
+def invert(lst):
+    """Given a set of numbers, return the additive inverse of each.
+    Each positive becomes negatives, and the negatives become positives."""
+    return [-num for num in lst]
+
