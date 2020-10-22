f23b371afd517d618414dbe16d7f343398e33ea8
neternefer <nodzynska@gmail.com>
2020-10-22 19:27:28 +0200

    Add xo kata

---
+++ xo.py
@@ -0,0 +1,6 @@
+def xo(s):
+    """Check to see if a string has the same amount of
+    'x's and 'o's. The method must return a boolean and be
+    case insensitive. The string can contain any char.
+    """
+    return True
