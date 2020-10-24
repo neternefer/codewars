619a01e81888a426cd93a3a28a24df3efa13846d
neternefer <nodzynska@gmail.com>
2020-10-24 19:51:15 +0200

    Add monkey_count kata

---
+++ monkey_count.py
@@ -0,0 +1,4 @@
+def monkey_count(n):
+    """Given the number (n), populate an array with all numbers up
+    to and including that number, but excluding zero."""
+    return [i for i in range(1, n + 1)]
