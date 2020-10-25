290b9592538ff7e4f7e44d839658e900b6ee894b
neternefer <nodzynska@gmail.com>
2020-10-25 20:07:40 +0100

    Add sum_array kata

---
+++ sum_array.py
@@ -0,0 +1,6 @@
+def sum_array(a):
+    """Write a method sum_array that takes an array of numbers and returns
+    the sum of the numbers.  The numbers can also be negative.
+    If the array does not contain any numbers then you should return 0.
+    """
+    return sum(a) if a else 0
