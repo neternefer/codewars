f63fefef8b258446899bd53f5db7cdb4e4ab43bf
neternefer <nodzynska@gmail.com>
2020-10-29 14:06:07 +0100

    Add no_odds kata

---
+++ no_odds.py
@@ -0,0 +1,6 @@
+def no_odds(values):
+    """Write a small function that returns the values of an array that are not odd.
+    All values in the array will be integers. Return the good values in the order
+    they are given.
+    """
+    return [item for item in values if item % 2 == 0]
