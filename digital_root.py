c25f789db8bbf9437ddcb6b551fb267cff87d107
neternefer <nodzynska@gmail.com>
2020-11-03 15:47:40 +0100

    Add digital_root kata

--- digital_root.py
+++ digital_root.py
@@ -3,6 +3,9 @@ def digital_root(n):
     If that value has more than one digit, continue
     reducing in this way until a single-digit number is produced.
     The input will be a non-negative integer.
+    Iterative version:
+    def digital_root(n):
+    return n if n < 10 else digital_root(sum(map(int,str(n))))
     """
     if n % 9 == 0 and n != 0:
         return 9
