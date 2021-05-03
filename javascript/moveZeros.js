fa76a561cfb6f2e26fb5a18bae75bffa9713ab17
neternefer <nodzynska@gmail.com>
2021-05-03 11:52:22 +0200

    Add moveZeros kata

---
+++ javascript/moveZeros.js
@@ -0,0 +1,12 @@
+//Write an algorithm that takes an array and moves all of
+//the zeros to the end, preserving the order of the other elements.
+//moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+var moveZeros = function (arr) {
+  arr.forEach((el) => {
+    if (el === 0) {
+      arr.splice(arr.indexOf(el), 1);
+      arr.push(0);
+    }
+  });
+  return arr;
+};
