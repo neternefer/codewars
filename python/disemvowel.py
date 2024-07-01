d620472532b7292ead567c7e4e587eea5f5d5def
shutruknahunte <alkarinqe@yahoo.com>
2024-07-01 15:21:13 +0200

    Improve disemvowel kata solution

--- python/disemvowel.py
+++ python/disemvowel.py
@@ -2,8 +2,5 @@ def disemvowel(string):
     """Your task is to write a function that takes a string
     and return a new string with all vowels removed.
     For this kata 'y' isn't considered a vowel."""
-    new_str = ''
-    for letter in string:
-        if letter not in 'aeiouAEIOU':
-            new_str += letter
-    return new_str
+    
+    return "".join(letter for letter in string if letter.lower() not in 'aeiou')
